bd9a192b4b0bedc3c8e88812ee7b62ba4aac07ee
Shawn Anderson <shawnltf@pm.me>
2020-09-23 02:07:49 -0700

    Finally got the structure down for dotfiles with stow.

---
+++ dotfiles/.config/cmus/lib.pl
@@ -0,0 +1,136 @@
+/home/ygg/Music/1 Hour of the Best Professor Layton Music (Part 2)--g53oLQgmN8.opus
+/home/ygg/Music/Animals of The Earth Vol​.​1 (Ethnotronic, World, Ethnic, Tribal, folktronica, Acoustic, Chill Out)-bWrNxOA5NI4.opus
+/home/ygg/Music/a storm of songs _ zelda ost + thunderstorm ambience-3FzJHsri8Zw.opus
+/home/ygg/Music/Avatar - The Last Airbender Theme _ EPIC VERSION-6N9SS6L0TDU.opus
+/home/ygg/Music/'Back To The 80's' _ Best of Synthwave And Retro Electro Music Mix for 1 Hour _ Vol. 16-XI5Bvplb7pQ.opus
+/home/ygg/Music/Beautiful & Relaxing The Legend of Zelda Music-DM8gE6EMwhE.opus
+/home/ygg/Music/Best Drum & Bass Mix 2020 (Melodic_Liquid Drum and Bass)-7j4WQ5qOBZY.opus
+/home/ygg/Music/Buddha Deep Club (Compiled by Salvo Migliorini)-l1slyzGQ1yc.opus
+/home/ygg/Music/Burning Man 2015   Deep Tunes for Deep playa Vol5-A-jUKBd0zF4.opus
+/home/ygg/Music/Camo & Krooked - Loa-kYfua2wRajY.opus
+/home/ygg/Music/Carbon Based Lifeforms - Derelicts _ Full Album-5o_uF1L5l6o.mkv
+/home/ygg/Music/Carbon Based Lifeforms - Derelicts _ Full Album-5o_uF1L5l6o.opus
+/home/ygg/Music/CharlestheFirst - Out of the Dark (45 Minutes of Original Music)-fhpA9Mp9_xc.webm
+/home/ygg/Music/Charlotte de Witte ‘New Form’ Livestream-3cOOu52n26c.opus
+/home/ygg/Music/Chill Out Music Mix 🌷 Best Chill Trap, Indie, Deep House ♫-rj73lCWeZQw.opus
+/home/ygg/Music/CloZee 4_26_2020 Full Set-5_rlxHXY0pI.mp3
+/home/ygg/Music/CloZee - Lollapalooza 2020 Set-j48gxPylyqo.opus
+/home/ygg/Music/Cosmic Flux - Psychill, PsyTrance, Psybient Mix-grWRSyptTnk.opus
+/home/ygg/Music/Creative Music vol 01 - Dwarves of the First Age-GNwv4cH-i5k.opus
+/home/ygg/Music/Entheogenic - Animism [2020] (Full Album Tryptology Mixtape)-oDlxH2Jl3rs.opus
+/home/ygg/Music/Fantasy Bard_Tavern Music Compilation-wLlovxa3VJ0.opus
+/home/ygg/Music/Fernanda Pistelli @ Garden Sessions for 5uinto-DokOdAjPtLw.m4a
+/home/ygg/Music/Final Fantasy ~ Jazz _ Brass Mix-7nI8Xuy363k.opus
+/home/ygg/Music/Final Fantasy VIII - Complete Soundtrack-X6kAwpgW29M.opus
+/home/ygg/Music/Flamingo’s healing journey-dN0uIGVi04U.opus
+/home/ygg/Music/Full Hour of Avatar the Last Airbender and Korra Amazing Soundtracks!--_727Gt43xE.opus
+/home/ygg/Music/Ghost of Me   A Chill Trap and Future Bass Mix-7hpHZGOaNs0.opus
+/home/ygg/Music/Globular & Geoglyph - Messages From The Resonator (Psychedelic Dub _ World Music)-Nq6M_Lod9RQ.mp3
+/home/ygg/Music/Indie_Pop_Folk Compilation - August 2020 (1½-Hour Playlist)-pNfhQsMeIP4.opus
+/home/ygg/Music/Jackson Lewis - Found In Love (Mose, San Miguel & more Remixes) [Folktronica]-hBDQ2FSRxnA.opus
+/home/ygg/Music/Jakare - Apu (EP Mix) [Folktronica _ Downtempo]-c6rkYrwudww.opus
+/home/ygg/Music/Jakare - Schizophonia (EP Mix) [Folktronica _ Shamanic Downtempo]-yFv6t9_-_24.f251.webm
+/home/ygg/Music/Jakare - Schizophonia (EP Mix) [Folktronica _ Shamanic Downtempo]-yFv6t9_-_24.opus
+/home/ygg/Music/Jakare - Schizophonia (EP Mix) [Folktronica _ Shamanic Downtempo]-yFv6t9_-_24.webm
+/home/ygg/Music/Jules Urbach explains OTOY's real-time graphics rendering-sCLFaoDhQPU.m4a
+/home/ygg/Music/Kingdom Hearts 2 OST Collection-d7b-cpSpS9A.opus
+/home/ygg/Music/Leaves From The Vine (Little Soldier Boy) _ CINEMATIC ORCHESTRAL VERSION 🍃-vu9Bl4ZMOd0.opus
+/home/ygg/Music/Lee Jones - A Perfect Kick (Matthias Meyer Remix)-XtriEg3neSg.opus
+/home/ygg/Music/Liquid Bloom - Live @ Chill Out Gardens - Boom Festival 2018-uiYk1d9jL_w.opus
+/home/ygg/Music/Moontricks - Home-BhAyt7g5tNs.opus
+/home/ygg/Music/Mose - Cacao Dance @ Eagles Nest Atitlán-wKI-ngRQJ90.opus
+/home/ygg/Music/NUMA  - Fuego Sagrado (Mix) [Folktronica _ Tribal Downtempo _ Shamanic]-BKPQ_S71poE.opus
+/home/ygg/Music/Ollie Mundy - Come Down with Me - The Full Moon Mix-X-KbDIfiRoI.opus
+/home/ygg/Music/Relaxing and Emotional Zelda Music Mix (2020)-imO26FzxRro.opus
+/home/ygg/Music/Relaxing The Legend of Zelda Jazz Covers--4OxmD4AocU.opus
+/home/ygg/Music/Relaxing The Legend of Zelda - The Wind Waker Music-0r12JCTpEt0.opus
+/home/ygg/Music/Sahalé - Djiin (6 hours)-nIsrrgF-xGE.opus
+/home/ygg/Music/Sanctuary _ Chillstep Mix-0CDbtwIiW3A.opus
+/home/ygg/Music/Seba - Painted Skies (Parallax Extended Remix)-zswXWHqzFkM.opus
+/home/ygg/Music/Tales Of Symphonia Complete Soundtrack-o_PCgbplpj4.opus
+/home/ygg/Music/The Last Agni Kai - Avatar the Last Airbender - One Hour-bLkQU60B-IA.opus
+/home/ygg/Music/tribe_vibe/Brian Cid - Margarette (Buddha Bar by Armen Miran & Ravin 2017) (Original Mix)-oK-vkCEiwOI.mp3
+/home/ygg/Music/tribe_vibe/Deya Dova - Return Of The Bird Tribes (saQi Remix)-DwT59_MfvyU.mp3
+/home/ygg/Music/tribe_vibe/Ethnic Deep House Vol#10 (Armen Miran _Davi_ Oceanvs Orientalis )-134NlN8tdyc.mp3
+/home/ygg/Music/tribe_vibe/Gerra G - A Mascara (Rodrigo Gallardo Remix) _ Exotic Refreshment LTD-ch8T2EULjxU.mp3
+/home/ygg/Music/tribe_vibe/Go Dugong - Vidita (El Búho Remix)-ealqZDkQqXY.mp3
+/home/ygg/Music/tribe_vibe/Honra (Original)-nDq-r2X9uFI.mp3
+/home/ygg/Music/tribe_vibe/Ipek Ipekcioglu feat. Petra Nachtmanova - Uyan Uyan  _ Kater130-9-ZsLSxWuhw.mp3
+/home/ygg/Music/tribe_vibe/Jacob Groening, Zigan Aldi - Checkpoint-qKVtqwiRAdQ.mp3
+/home/ygg/Music/tribe_vibe/Kazuma Akasaki - I Can Find You [Melodic Techno House Mix]-NfOGsZwwdjg.mp3
+/home/ygg/Music/tribe_vibe/Layne Redmond & Tommy Brunjes - Morrocan Moon-YqmJoiHj8NE.mp3
+/home/ygg/Music/tribe_vibe/Lee Jones - A Perfect Kick (Matthias Meyer Remix)-XtriEg3neSg.mp3
+/home/ygg/Music/tribe_vibe/Lee Jones - One Grain-mIAtmjq0jC4.mp3
+/home/ygg/Music/tribe_vibe/Moontricks  - Solar Therapy-u2ltkcGOz_c.mp3
+/home/ygg/Music/tribe_vibe/Mumbai Madness-bzXgX_V0Q0Y.mp3
+/home/ygg/Music/tribe_vibe/NenaHelena - La Ka Ruba Ft. Akil [E008]-amj-LmHyGXM.mp3
+/home/ygg/Music/tribe_vibe/Nostalgia (Extended)-L-3sPvjaUzM.mp3
+/home/ygg/Music/tribe_vibe/Oceanvs Orientalis - Khronos (Full EP)-mDGCSZ8VTuw.mp3
+/home/ygg/Music/tribe_vibe/Peter Power - Sun Sun Damba [Multi Culti]-aRC_7AE8DhI.mp3
+/home/ygg/Music/tribe_vibe/Raz Ohara - El Zahir (Original Mix)-wKDmzUYKV9Q.mp3
+/home/ygg/Music/tribe_vibe/Reaching Rujum-mYM4hasoo4o.mp3
+/home/ygg/Music/tribe_vibe/Rej Senhor (Original Version)-fBkVbYDLJDM.mp3
+/home/ygg/Music/tribe_vibe/Roots of Wisdom (Original)-e8luC8BR9i8.mp3
+/home/ygg/Music/tribe_vibe/Satori (NL) feat. Miou Amadée - Days Without You (Crussen Remix)-qFMEKS0ZWn0.mp3
+/home/ygg/Music/tribe_vibe/Theme For Namgar (Crussen Edit)-5oHPgSuKLZo.mp3
+/home/ygg/Music/tribe_vibe/Timboletti  - Helva Selva (feat.  Ziski & Pophop Remix)-pAcFzVFzjKs.mp3
+/home/ygg/Music/tribe_vibe/Traumer - Classroom-fyKY3tfyAVY.mp3
+/home/ygg/Music/tribe_vibe/Unspoken Word (Intro)-ePayp_lvzV8.mp3
+/home/ygg/Music/tribe_vibe/Vesaire-lLFYPPGcG-Q.mp3
+/home/ygg/Music/tribe_vibe/Vidita Ft. Miriam García (El Buho Remix)-o2TgDfnPmZI.mp3
+/home/ygg/Music/tribe_vibe/War Cry-J2sZUeasZkY.mp3
+/home/ygg/Music/tribe_vibe/Yet Another Day Thinking About You-zEOe-ckZjJc.mp3
+/home/ygg/Music/tribe_vibe/YokoO - Yet Another Day Thinking About You feat  JoKe & Mauve [ADIDA001]-xOss6oP8xiY.mp3
+/home/ygg/Music/Xenia - Live @ Radio Intense Kyiv 7.4.2020 _ Techno Mix-r3DMXRWcmCI.opus
+/home/ygg/Music/yoga/Grounding Into Gratitude - Root Chakra Yoga - Yoga With Adriene-0pbuvhbg7po.f251.webm
+/home/ygg/Music/yoga/Grounding Into Gratitude - Root Chakra Yoga - Yoga With Adriene-0pbuvhbg7po.mkv
+/home/ygg/Music/yoga/Grounding Into Gratitude - Root Chakra Yoga - Yoga With Adriene-0pbuvhbg7po.opus
+/home/ygg/Music/yoga/Grounding Root Chakra Guided Meditation - Stress Relief Meditation _ Chakra Challenge-cX6b6v1k0QQ.opus
+/home/ygg/Music/Zelda Music To Relax_Study_Work_Game-W5AZeNGB6Ds.opus
+/home/ygg/Music/Zelda Ocarina of Time - MASSIVE MEDLEY! - Super Guitar Bros-NFw-FrYmAEw.opus
+/home/ygg/Music/zenaida/audio/Born this way cover - lady Zenaida-vrUs96lg_oA.m4a
+/home/ygg/Music/zenaida/audio/“Clean up the Ocean” DNCE re-write cover-yeTdj45wbLo.m4a
+/home/ygg/Music/zenaida/audio/Dream a little Dream - DIY Bee Condo-wgF-lzB_lSQ.m4a
+/home/ygg/Music/zenaida/audio/'Friend Like Me' Cover - Lady Zenaida-eHK-u1-nLq8.opus
+/home/ygg/Music/zenaida/audio/Lady Zenaida «  Everybody wants to be a Cat » Cover-PazfkBSaubI.m4a
+/home/ygg/Music/zenaida/Born this way cover - lady Zenaida-vrUs96lg_oA.m4a
+/home/ygg/Music/zenaida/Zenaida_FB_Altspace_Concert_10000000_249474506347465_7360425835515086942_n.mp4
+/home/ygg/Music/zenaida/Zenaida_FB_Altspace_Concert_10000000_335146147822030_4963841038274551466_n.mp4
+/home/ygg/Music/654-BitcoinIsPsychedelic.mp3
+/home/ygg/Music/Summer Firefly feat. Deepakalypse (Prod. Stephan Jacobs).mp3
+/home/ygg/Music/Electro House 2016 Best Festival Party Video Mix. New EDM, Dance Charts Songs. Club Music Remix.mp3
+/home/ygg/Music/zenaida/audio/Humanite Uni Toi - demo.mp3
+/home/ygg/Music/zenaida/Humanite Uni Toi - demo.mp3
+/home/ygg/Music/zenaida/audio/Rainbow Warriors -Demo.mp3
+/home/ygg/Music/zenaida/Rainbow Warriors -Demo.mp3
+/home/ygg/Music/zenaida/audio/Save Me From Myself.mp3
+/home/ygg/Music/zenaida/Save Me From Myself.mp3
+/home/ygg/Music/zenaida/audio/Mastered- Let It Go -Lady ZenAida.mp3
+/home/ygg/Music/zenaida/Mastered- Let It Go -Lady ZenAida.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 1 Ethan Buchman.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 2 John Henry Clippinger.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 3 Chris Goes.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 4 Daniel Christian Wahl.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 5 Jack Zampolin.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 6 Delton Chen.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 7 Dan Mapes.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 8 Martin Wainstein.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Current Events Special with Rhamis Kent.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 10 Cosmuhammad Bitcoin Jaesustein.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 11 David McConville and Dawn Danby.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 12 Joe Brewer.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 13 Amanda Joy Ravenhill.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 14 John Henry Clippinger (Encore).mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 15 Coronavirus Special.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 16 Loren Cardeli.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 17 Dr. Dorn Cox.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 18 Larry Kopald.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 19 Tom Newmark.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 20 Michel Bauwens.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 21 Frank Van Gansbeke.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 23 Jim Rutt.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 24 Wenzl McGowen.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 25 Jeff Emmett.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 26 Shaun Conway.mp3
+/home/ygg/Music/PlanetaryRegenerationPodcast/Planetary Regeneration Podcast  Episode 9 Neal Spackman.mp3
+/home/ygg/Music/VibeSquaD-and-ProjectAspect-LITTLE-DAB (2013).mp3
