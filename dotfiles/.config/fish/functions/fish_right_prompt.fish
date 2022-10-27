9483a86f0786d7cd13e277550287538c00ed0553
Shawn Anderson <shawn@longtailfinancial.com>
2022-10-27 15:11:44 -0700

    Create right fish prompt with time and watch emoji. Add spaces to match tmux power.

---
+++ dotfiles/.config/fish/functions/fish_right_prompt.fish
@@ -0,0 +1,19 @@
+# fish theme: gentoo
+
+function fish_right_prompt
+  set -l last_status $status
+  set -l cyan (set_color -o cyan)
+  set -l red (set_color -o red)
+  set -l normal (set_color normal)
+
+  # print last exit code if nonzero:
+  if test $last_status -ne 0
+   set_color red
+   printf '%d' $last_status
+   set_color normal
+  end
+
+  # display the timestamp on the utmost right.
+  echo -n -s $normal '⌚ ' (date +%H:%M:%S)  ' '
+end
+
