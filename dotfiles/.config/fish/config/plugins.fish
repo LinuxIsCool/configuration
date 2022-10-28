af8c80917f2d7da705ab5f2ce816163c87ca90e0
Shawn Anderson <shawn@longtailfinancial.com>
2022-10-28 09:06:20 -0700

    Add default node version.

--- dotfiles/.config/fish/config/plugins.fish
+++ dotfiles/.config/fish/config/plugins.fish
@@ -1,3 +1,7 @@
-
 # jichu4n/fish-command-timer
+# Don't display date or time as I already have that in tmux power and in right_fish_prompt
 set fish_command_timer_time_format ''
+
+# jorgebucaran/nvm.fish
+# Set default version for node so that it is available by default.
+set nvm_default_version v18.4.0
