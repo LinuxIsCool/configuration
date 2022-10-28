0e9b6d583fd151d81cac265e1292b75ecd7512ab
Shawn Anderson <shawn@longtailfinancial.com>
2022-10-28 10:19:29 -0700

    Add minimum timing to fish command timer.

--- dotfiles/.config/fish/config/plugins.fish
+++ dotfiles/.config/fish/config/plugins.fish
@@ -1,6 +1,7 @@
 # jichu4n/fish-command-timer
 # Don't display date or time as I already have that in tmux power and in right_fish_prompt
 set fish_command_timer_time_format ''
+set fish_command_timer_min_cmd_duration 500
 
 # jorgebucaran/nvm.fish
 # Set default version for node so that it is available by default.
